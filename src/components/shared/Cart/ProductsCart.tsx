da54975e4d5d2d722ba717f8669fd2e7ebe9f6be
blessed <qx.dicawl@gmail.com>
2025-12-30 12:07:23 +0200

    fix imports

--- src/components/shared/Cart/ProductsCart.tsx
+++ src/components/shared/Cart/ProductsCart.tsx
@@ -2,9 +2,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Box, Divider } from '@mui/material';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
+
 import greyBox from '/icons/greyBox.png';
 import TrashIcon from '/icons/trash.svg';
-
 import type { RootState } from '../../../core/store';
 import {
   decreaseAmount,
