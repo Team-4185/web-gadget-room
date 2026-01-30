8ccc6c62ffb5b25c44d89d5a184085a7bcf4ea2e
Yaroslav Tsybukh <yaroslav.tsybukh.development@gmail.com>
2026-01-30 16:10:40 +0200

    responsive settings

--- src/core/config/theme.ts
+++ src/core/config/theme.ts
@@ -4,7 +4,39 @@ export const theme = createTheme({
   typography: {
     fontFamily: "'Montserrat', sans-serif",
   },
+  breakpoints: {
+    values: {
+      xs: 375,
+      sm: 768,
+      md: 900,
+      lg: 1200,
+      xl: 1440,
+    },
+  },
   components: {
+    MuiContainer: {
+      defaultProps: {
+        maxWidth: false,
+      },
+      styleOverrides: {
+        root: ({ theme }) => ({
+          maxWidth: '1280px',
+
+          [theme.breakpoints.down('xl')]: {
+            maxWidth: '1140px',
+          },
+          [theme.breakpoints.down('lg')]: {
+            maxWidth: '860px',
+          },
+          [theme.breakpoints.down('md')]: {
+            maxWidth: '670px',
+          },
+          [theme.breakpoints.down('sm')]: {
+            width: '100%',
+          },
+        }),
+      },
+    },
     MuiButton: {
       styleOverrides: {
         root: {
