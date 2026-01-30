1f1dccaaa2961a9f273461d510de9c269027b3c6
Yaroslav Tsybukh <yaroslav.tsybukh.development@gmail.com>
2026-01-30 16:32:56 +0200

    removing the component

--- src/components/Catalog/index.ts
+++ src/components/Catalog/index.ts
@@ -2,4 +2,3 @@ export { CatalogFilters } from './CatalogFilters/CatalogFilters';
 export { CatalogContent } from './CatalogContent/CatalogContent';
 export { CatalogHeader } from './CatalogHeader';
 export { CatalogPagination } from './CatalogPagination';
-export { CatalogProducts } from './CatalogProducts';
