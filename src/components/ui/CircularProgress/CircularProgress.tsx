ba6fb25d386a7cdf372f452d565533296df40f8a
Yaroslav Tsybukh <yaroslav.tsybukh.development@gmail.com>
2026-03-19 18:54:38 +0200

    creating the CircularProgress component

---
+++ src/components/ui/CircularProgress/CircularProgress.tsx
@@ -0,0 +1,11 @@
+import { CircularProgress as CircularProgressMUI } from '@mui/material';
+
+import './CircularProgress.css';
+
+export const CircularProgress = () => {
+  return (
+    <div className="circularProgress">
+      <CircularProgressMUI />
+    </div>
+  );
+};
